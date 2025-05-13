2aee936bfad637b4f9685c6fcbfc47a6ccf7d6d6
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-05-14 02:21:39 +0600

    Content

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Banner from "./components/Banner/Banner";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header"
-
+import Content from "./components/Content/Content"
 function App() {
   return (
     <>
-      <Header>
-        <Navbar></Navbar>
-        <Banner></Banner>
-      </Header>
+        <Header>
+          <Navbar></Navbar>
+          <Banner></Banner>
+        </Header>
+        
+        
+        <Content></Content>
+      
     </>
   );
 }
---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import Container from "../Layout/Container"
+import contenticon1 from "../../assets/contenticon1.png"
+import contenticon2 from "../../assets/contenticon2.png"
+import contenticon3 from "../../assets/contenticon3.png"
+const Content = () => {
+
+
+  const item = [{
+    img: contenticon1,
+    titel: "Login or sign up to be able use our platform",
+    description: "This quickstart shows you how to use Identity Platform to sign in a user with an email and password.",
+  },
+
+  {
+    img: contenticon2,
+    titel: "Connect your website with just a few click",
+    description: "Once your website is online, you can configure it, I will show you how to put your website online.",
+  },
+
+  {
+    img: contenticon3,
+    titel: "Take some sales data that you want",
+    description: "Sell your data directly: The most straightforward method is to sell your data directly to another.",
+  }
+  ]
+
+
+  return (
+
+    <div className="pt-[336px] pb-[160px]">
+      <Container>
+
+        <div className='px-[50px]'>
+          <div className='flex justify-between'>
+            <h2 className='w-[504px] font-primary font-bold text-[46px] leading-[58px] text-title'>How simple is it to use our platform?</h2>
+            <p className='w-[530px] font-secondary text-lg leading-[32px] text-discription'>This Innovate Con guide explores the most popular platforms and walks you through how to use them to grow your business.</p>
+          </div>
+        </div>
+
+        {/* <div className='w-[300px]  flex  flex-col items-center' >
+              <img className='w-[110px]' src={contenticon1} alt="#icon" />
+              <h4 className='font-primary font-bold text-[24px] leading-[36px] text-title'>Login or sign up to be able use our platform</h4>
+              <p className='font-secondary  text-lg leading-[32px] text-description'>This quickstart shows you how to use Identity Platform to sign in a user with an email and password. </p>
+            </div> */}
+
+            <div className='flex justify-around items-center text-center mt-[116px]'>
+                 {
+           item.map((item, index) => (
+            <div key={`First-${index}`} className='w-[300px] flex flex-col items-center'>
+              <img className='w-[110px]' src={item.img} alt="#" />
+              <h4 className='font-primary font-bold text-[24px] leading-[36px] text-title'>{item.titel}</h4>
+              <p className='font-secondary text-lg leading-[32px] text-description'>{item.description}</p>
+            </div>
+          ))
+        }
+            </div>
+       
+      
+
+      </Container>
+
+    </div>
+  )
+}
+
+export default Content
